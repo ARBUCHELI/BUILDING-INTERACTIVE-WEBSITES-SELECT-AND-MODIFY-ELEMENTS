4d59d79e8b489dc73821915805521ae9cdc107d3
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2022-05-19 10:18:55 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+document.querySelector('h1').innerHTML = '<h1>Most popular Harry Potter characters</h1>';
+
+document.getElementById('fourth').innerHTML = 'Professor Snape';
+
+document.getElementsByClassName('slytherin')[0].innerHTML = 'Salazar Slytherin';
+
+document.getElementsByTagName('li')[0].innerHTML = 'Dobby';
